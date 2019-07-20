a3bc4aef4d7d295cf6ae5ef45c3d95ab61ad20b5
nitulsahu <AnakinSkywalker@example.com>
2019-07-20 23:40:55 +0500

    Added the getter and setter methods

--- Book.java
+++ Book.java
@@ -5,6 +5,16 @@ private String title;
 private String aurthor;
 private flaote price;
 
+public void setBookID(int bookId){
+this.bookId=bookId;
+}
+public int getbookID()
+{
+return bookId;
+}
+public void setTitle(String title){
+this.title=title;
+}
 public static void main(String args[])
 {
 System.out.println("hello sir");

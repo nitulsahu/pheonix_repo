31a128dfdb2ddf335aaf64c0da85ac847ab04206
nitulsahu <AnakinSkywalker@example.com>
2019-07-21 04:21:07 +0500

    the nsbranch has been made and file has been updated here

--- Book.java
+++ Book.java
@@ -12,15 +12,26 @@ public int getbookID()
 {
 return bookId;
 }
+
 public void setTitle(String title){
 this.title=title;
 }
+
 public static void main(String args[])
 {
 System.out.println("hello sir");
 }
+
 public void setAuthor(String author)
 {
 this.author=author;
 a/lskdjf;lkdsajf
 }
+
+public void addDetails()
+{
+sout ("hello this is the age of science");
+}
+
+
+sopiutadskf;l'dajfldsajflkdsajf;ldsajfa;ldsfj;ldsakgjfs;ldkfj;lkdsfj;lkdsajf;lkdsajf;lkdsajf;lkdsajfa;lkdsjf;lkdsajfa;lsdjf;ldsafj;ldsakjfsa;lkdjf;ldsakj
